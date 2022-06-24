fe6b7047e5f4cb827ad782380a3127cd0b7e6a85
David <david.lazaro@babelgroup.com>
2022-06-24 14:10:43 +0200

    sin arreglar buscar

--- src/app/galerias/pages/buscar/buscar.component.ts
+++ src/app/galerias/pages/buscar/buscar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Imagen } from 'src/app/interfaces/imagen-interface';
+
 
 @Component({
   selector: 'app-buscar',
@@ -7,39 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-
   termino: string = '';
+  imagenes: Imagen[] = []; // las imágenes de la galeria
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-/*
-  buscando() {
-    this.servicioHeroes
-      .getSugerencias(this.termino.trim())
-      .subscribe((heroes) => (this.heroes = heroes));
-  }
+  opcionSeleccionada(termino: string) {
 
-  opcionSeleccionada(galeria: MatAutocompleteSelectedEvent) {
-    if (!galeria.option.value) {
+    console.log(termino);
+    if (!termino) {
       // mostrar un pop up sin cambiar el contenido actual
       // this.heroeSeleccionado = undefined;
       return;
     }
-
-    // recuperamos qué galeria es ahora la actual. OJO, va por título
-    const index = this.galerias.findIndex(value => value.titulo_galeria === galeria.option.value);
-    console.log(index);
-    if (index >= 0) {
-      this.galeriaActual = this.galerias[index];
-    } else {
-      // mostrar un pop up sin cambiar el contenido actual
-    }
-
-    // comprobar que existe el valor introducido
-    this.getImagesGal(this.galeriaActual.directorio_galeria);
   }
-*/
+
 }
